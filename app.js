49375ad07a85d34be4a4183b8b2fd18b08c6bfb8
kiprutobrian <briankipruto793@gmail.com>
2021-11-20 13:19:37 +0300

    clean up

--- app.js
+++ app.js
@@ -18,70 +18,6 @@ let smsChannel = {
 
 const purseId = process.env.PURSE_ID;
 
-const clientPayment = async (customer, amount) => {
-  log.info(`Processing loan for ${customer.customerNumber.number}`);
-
-  const { name, day } = await customer.getMetadata();
-
-  const res = await client.initiatePayment(
-    {
-      purseId,
-    },
-    {
-      channelNumber: mpesaChannel,
-      customerNumber: customer.customerNumber,
-    },
-    {
-      amount,
-      currencyCode: "KES",
-    }
-  );
-  if (
-    ![
-      "success",
-      "queued",
-      "pending_confirmation",
-      "pending_validation",
-    ].includes(res.status)
-  ) {
-    log.error(
-      `Failed to send KES ${amount} to ${customer.customerNumber.number} --> ${res.status}: `,
-      res.description
-    );
-    return;
-  }
-  await customer.updateMetadata({
-    name,
-    fee: amount,
-  });
-  await customer.sendMessage(smsChannel, {
-    body: {
-      text: `Congratulations ${name}!\nYour fee of KES ${amount} has been received succesfully!`,
-    },
-  });
-};
-
-async function sendResults(num, day) {
-  let telegramChannel = {
-    channel: "telegram",
-    number: "kibandi",
-  };
-  const patient = new client.Customer({
-    number: num,
-    provider: "cellular",
-  });
-  await patient
-    .sendMessage(smsChannel, {
-      body: {
-        text: `Your appointment has been scheduled. Thank You for your time. See You on ${day}! :-)`,
-      },
-    })
-    .catch((error) => {
-      console.log(error.message);
-      console.log("message not sent......");
-    });
-}
-
 const processUssd = async (notification, customer, appData, callback) => {
   let patientNumber = notification.customerNumber.number;
   console.log(notification.customerNumber.number);
@@ -150,32 +86,6 @@ const processUssd = async (notification, customer, appData, callback) => {
         callback(menu, {
           screen: nextScreen,
         });
-        break;
-      case "schedule-day":
-        name = input;
-        menu.text = `What day would you like to schedule ${name}\n1. Monday\n2. Tuesday\n3. Wednesday\n4. Thursday\n5. Friday`;
-        nextscreen = "confirm-day";
-        callback(menu, {
-          screen: nextscreen,
-        });
-        break;
-      case "confirm-day":
-        let value = input;
-
-        week = {
-          1: "Monday",
-          2: "Tuesday",
-          3: "Wednesday",
-          4: "Thursday",
-          5: "Friday",
-        };
-        day = week[value];
-        menu.text = `Thank you,\n we will get back to you on the availability of ${day}. You will be charged 1000 bob for consultation!`;
-        menu.isTerminal = true;
-        nextscreen = "home";
-        callback(menu, { screen: nextscreen });
-        await sendResults(patientNumber, day);
-        await clientPayment(customer, 1000);
         break;
       case "quit":
         menu.text = "Happy Coding!";
